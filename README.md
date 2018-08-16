5635545c8e063f8eb711e7300c6e94983c662c3c
iGor Polyakov <igor_polyakov@phystech.edu>
2018-08-17 00:48:29 +0300

    sails added

--- README.md
+++ README.md
@@ -1,23 +1,11 @@
-# Angular Template
+# Online Tutoring
 
-This is an SPA workspace template which contains modern JavaScript frameworks and libraries specialized for immediate development a new application.
-The list of technologies:
+Add a `local.js` file for development needs
+```
+./run gen:local
+```
 
-- Angular 6 with options
-  - SCSS
-  - no-tests
-  - router
-- Bootstrap 4
-- jQuery
-- Font Awesome 5
-- Markdown
-- gh-pages
-
-
-## Installation
-
-```sh
-git clone https://github.com/polyakovin/template_angular.git && \
-cd template_angular && \
-sh switch_repo.sh <your_repo_name>
+For development and deployment
+```
+./run help
 ```
---
+++ config/session.js
@@ -0,0 +1,41 @@
+/**
+ * Session Configuration
+ * (sails.config.session)
+ *
+ * Use the settings below to configure session integration in your app.
+ * (for additional recommended settings, see `config/env/production.js`)
+ *
+ * For all available options, see:
+ * https://sailsjs.com/config/session
+ */
+
+module.exports.session = {
+
+  /***************************************************************************
+  *                                                                          *
+  * Session secret is automatically generated when your new app is created   *
+  * Replace at your own risk in production-- you will invalidate the cookies *
+  * of your users, forcing them to log in again.                             *
+  *                                                                          *
+  ***************************************************************************/
+  secret: '40b3bec2c8a4f7c374ff5b9119d5e4f9',
+
+  cookie: {
+    secure: false
+  }
+  /***************************************************************************
+  *                                                                          *
+  * Customize when built-in session support will be skipped.                 *
+  *                                                                          *
+  * (Useful for performance tuning; particularly to avoid wasting cycles on  *
+  * session management when responding to simple requests for static assets, *
+  * like images or stylesheets.)                                             *
+  *                                                                          *
+  * https://sailsjs.com/config/session                                       *
+  *                                                                          *
+  ***************************************************************************/
+  // isSessionDisabled: function (req){
+  //   return !!req.path.match(req._sails.LOOKS_LIKE_ASSET_RX);
+  // },
+
+};
