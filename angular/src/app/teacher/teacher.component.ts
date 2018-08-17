668a90a8d13f8ab934b0bd8e2b6e58b711e13089
iGor Polyakov <igor_polyakov@phystech.edu>
2018-08-17 13:40:05 +0300

    teacher added

--- angular/src/app/teacher/teacher.component.ts
+++ angular/src/app/teacher/teacher.component.ts
@@ -6,6 +6,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./teacher.component.scss']
 })
 export class TeacherComponent implements OnInit {
+  features = [
+    {
+      title: 'Официальное трудоустройство',
+      text: 'Накопление трудового стажа в школе или в учебном центре.',
+      img: 'icons-teacher-01'
+    },
+    {
+      title: 'Работа с опытными методистами',
+      text: 'Среди наших методистов, эксперты ОГЭ и ЕГЭ, члены жюри многих олимпиад РФ и Франции.',
+      img: 'icons-teacher-02'
+    },
+    {
+      title: 'Инновационность',
+      text: 'Наша система EdLABsis поможет существенно сократить рутинные процессы и сосредоточиться на главном.',
+      img: 'icons-teacher-03'
+    },
+    {
+      title: 'Профессиональный рост',
+      text: 'Наша программа рассчитана не только на подготовку к ЕГЭ, но и к уровневым и профильным олимпиадам.',
+      img: 'icons-teacher-04'
+    }
+  ]
 
   constructor() { }
 
